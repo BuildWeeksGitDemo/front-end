be3ffdee691854f02da5d657fb1116fe2100990d
bummings <eabummings@gmail.com>
2020-08-14 18:45:27 -0400

    ready to gooooo

--- buildweeksgitdemo/src/App.js
+++ buildweeksgitdemo/src/App.js
@@ -4,9 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>Hi!</p>
-      </header>
+      <h1 className='hello'>Hello!</h1>
     </div>
   );
 }
